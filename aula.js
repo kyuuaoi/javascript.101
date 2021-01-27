ba6be53fb5bcb9d32c5c7e03a86394c87aa239a1
kyuuaoi <gabrielluizon@gmail.com>
2021-01-27 03:35:10 -0300

    First Content

---
+++ aula.js
@@ -0,0 +1,11 @@
+let nome = "Gabriel"; // String Litteral
+let idade = 28; // Number Litteral
+let estaAprovado = true; // Boolean (regras)
+let sobreNome; // Undefined
+let corSelecionado = null // Null é mais usada para "redefinir valores"
+
+//
+//
+//
+
+
